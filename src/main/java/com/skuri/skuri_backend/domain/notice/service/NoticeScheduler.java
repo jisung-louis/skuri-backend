443c65b242c6ae9fdb50255e073364bf167765f4
jisung-louis <khkh9357@naver.com>
2026-04-02 16:04:57 +0900

    fix: 공지 스케줄러를 설정으로 제어 가능하게 변경

--- src/main/java/com/skuri/skuri_backend/domain/notice/service/NoticeScheduler.java
+++ src/main/java/com/skuri/skuri_backend/domain/notice/service/NoticeScheduler.java
@@ -2,10 +2,12 @@ package com.skuri.skuri_backend.domain.notice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "notice.sync.scheduler", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class NoticeScheduler {
 
     private final NoticeSyncService noticeSyncService;
